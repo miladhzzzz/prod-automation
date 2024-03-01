1b75dca9380d21f5577ef51a2532fab0d9d28c4e
milx <david.hacker09@gmail.com>
2024-03-01 20:44:31 +0330

    comments

--- init.sh
+++ init.sh
@@ -18,7 +18,7 @@ docker build -t prod-automation:latest . || {
     exit 1
 }
 
-# run docker container
+# run docker container PRIVILEGED MODE !!! we are mounting host os Docker Server to the automation container
 docker run -d --name prod-auto --privileged -v /var/run/docker.sock:/var/run/docker.sock -p 1111:1111 prod-automation:latest || {
     echo "Failed to run container..."
     exit 1
