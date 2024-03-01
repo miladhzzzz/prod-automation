fb1e0361c96cef83872b1c547bd1c5b7602814c8
milx <david.hacker09@gmail.com>
2024-03-01 19:14:20 +0330

    update init

--- init.sh
+++ init.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-docker run -d --privileged -v /usr/lib/systemd/system/docker.sock:/var/run/docker.sock -p 5001:5001 <your-image-name>
+
+docker build -t prod-automation:latest . || {
+    echo "Failed to build image..."
+    exit 1
+}
+
+docker run -d --name prod-auto --privileged -v /usr/lib/systemd/system/docker.sock:/var/run/docker.sock -p 1111:1111 prod-automation:latest || {
+    echo "Failed to run container..."
+    exit 1
+}
