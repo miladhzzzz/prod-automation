0e9dbb4f5ac41cb38e4510768f740cb2e8b1c175
milx <david.hacker09@gmail.com>
2024-03-01 20:12:16 +0330

    changing volume

--- init.sh
+++ init.sh
@@ -17,7 +17,7 @@ docker build -t prod-automation:latest . || {
 }
 
 # run docker container
-docker run -d --name prod-auto --privileged -v /usr/lib/systemd/system/docker.sock:/var/run/docker.sock -p 1111:1111 prod-automation:latest || {
+docker run -d --name prod-auto --privileged -v /var/run/docker.sock:/var/run/docker.sock -p 1111:1111 prod-automation:latest || {
     echo "Failed to run container..."
     exit 1
 }
