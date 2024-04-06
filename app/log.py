54b7dc37107480cc9b9e500f834166341b1cf1e4
milx <david.hacker09@gmail.com>
2024-04-06 13:18:58 +0330

    updated build log method to include commit_hash and trigger type

--- app/log.py
+++ app/log.py
@@ -1,17 +1,21 @@
 from db import ConnectionPool
 from typing import Optional, List, Dict
-import sqlite3, uuid, os
+import sqlite3, uuid, os, dockr, json
 
 LOGS_DIR = "build_logs"
 
 connection_pool = ConnectionPool()
 
-def log_build_request(project_name: str, status: str):
+def log_build_request(project_name: str, status: str, webhook: bool, commit_hash: str):
     try:
         with connection_pool.get_connection() as conn:
             cur = conn.cursor()
-            cur.execute("INSERT INTO jobs (id, project_id, status, log_file) VALUES (?, ?, ?, ?)", 
-                        (uuid.uuid4().hex, get_or_create_project_id(project_name), status, f"{project_name}.log"))
+            if webhook:
+                trigger = "webhook"
+            else:
+                trigger = "manual"   
+            cur.execute("INSERT INTO jobs (id, project_id, status, commit_hash, trigger, log_file) VALUES (?, ?, ?, ?, ?, ?)", 
+                        (uuid.uuid4().hex, get_or_create_project_id(project_name), status, commit_hash, trigger,f"{project_name}.log"))
             conn.commit()
     except sqlite3.Error as e:
         print(f"Error logging build request: {e}")
@@ -71,22 +75,43 @@ def update_project_counts(project_name: str, success: bool):
     except sqlite3.Error as e:
         print(f"Error logging build request: {e}")
 
-def get_env_vars(project_name):
+def get_jobs():
     try:
-        # Retrieve encrypted environment variables from the database
         with connection_pool.get_connection() as conn:
-            cursor = conn.cursor()
-            cursor.execute(
-                "SELECT variable_name, variable_value FROM project_environment_variables WHERE project_name = ?",
-                (project_name,)
-            )
-            records = cursor.fetchall()
+            cur = conn.cursor()
+            # Return the details of all jobs including project details
+            cur.execute('''SELECT j.id, j.status, j.commit_hash, j.trigger, j.log_file, p.name as project_name, p.success_count, p.failure_count 
+                        FROM jobs j
+                        JOIN projects p ON j.project_id = p.id''')
+            jobs = []
+            for row in cur.fetchall():
+                # Get container data associated with the project name
+                container_data = dockr.get_project_containers(row[3])
 
-        # if records:
-        #     # Decrypt the encrypted values and return the decrypted environment variables
-        #     decrypted_variables = {record[0]: decrypt_data(record[1]) for record in records}
-        #     return decrypted_variables
-              
-        return None
+                job = {
+                    "id": row[0],
+                    "status": row[1],
+                    "commit_hash": row[2],
+                    "trigger": row[3],
+                    "log_file": row[4],
+                    "project_name": row[5],
+                    "success_count": str(row[6]),  # Convert to string
+                    "failure_count": str(row[7])   # Convert to string
+                }
+
+                if row[1] == "success" and container_data:
+                    job["containers"] = json.dumps(container_data)
+                
+                if row[1] == "failed" and container_data:
+                    for container in container_data:
+                        if container.get("status") == "exited":
+                            container_logs = dockr.get_container_logs(row[3])
+                            job["container_name"] = container.get("name")
+                            job["container_status"] = container.get("status")
+                            job["container_logs"] = container_logs
+
+                jobs.append(job)
+            return jobs
+        
     except sqlite3.Error as e:
-        raise "Error retrieving environment variables: {e}"
+        print(f"Error logging build request: {e}")
